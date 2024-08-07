c908c5a7191f14adc7cabdfe39ef1145edc77049
separeit894 <rinatramazanov894@gmail.com>
2024-08-07 23:43:40 +0300

    update 2.1

--- main.py
+++ main.py
@@ -15,16 +15,7 @@ c.execute("""CREATE TABLE articles (
 )""")
 
 # Вводим значения в таблицу
-c.execute("INSERT INTO articles VALUES ('stopgame and youtube', 'стопгейм заблокируют сегодня', 100, 'Admib')")
-# c.execute("INSERT INTO articles VALUES ('youty block', 'qer', 100, 'sdfa'")
-
-# c.execute("""
-#     INSERT INTO articles VALUES (
-#     'youtube block',
-#     'youtube really block',
-#     100,
-#     'separeit'
-#     )""")
+c.execute("INSERT INTO articles VALUES ('Youtube', 'Youtube замедляют на 70%', 100, 'News')")
 
 # c.execute("SELECT rowid, title FROM articles")
 # c.execute("DELETE FROM articles WHERE rowid > 1")
